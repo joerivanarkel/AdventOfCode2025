5590efce7cee2ff7b7e8eb3031b87c6d2bc7126a
joerivanarkel <joeri@vanarkel.net>
2025-12-04 13:34:29 +0100

    Dag 1

---
+++ Dag1/Dag1/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+
+let mutable dial = 50
+let mutable answer = 0
+
+let input = File.ReadAllLines("input.txt")
+for line in input do
+    let direction = line.[0]
+    let distance = line.[1..] |> int
+
+    for i in 1 .. abs distance do
+        match direction with
+        | 'L' -> dial <- dial - 1
+        | 'R' -> dial <- dial + 1
+
+        if dial = 0 then
+            answer <- answer + 1
+
+        if dial = 100 then
+            dial <- 0
+            answer <- answer + 1
+        elif dial = -1 then
+            dial <- 99
+
+printfn "Answer: %i" answer
